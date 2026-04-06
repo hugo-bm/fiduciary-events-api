3bf8efa997f9d95bfb2b9b807df7a5e8d4cab78d
Hugo Batalha Moreno <hugomb123@gmail.com>
2026-04-06 20:38:50 -0300

    🐛 fix(model): enable factory support in User model using HasFactory

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 // use Database\Factories\UserFactory;
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 // use Illuminate\Foundation\Auth\User as Authenticatable;
 // use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
@@ -14,9 +14,9 @@ use Illuminate\Database\Eloquent\Model;
  *
  * Represents an authenticated system user with API access.
  */
-class User
+class User extends Model
 {
-    
+    use HasFactory;
     /**
      * The attributes that are mass assignable.
      *
