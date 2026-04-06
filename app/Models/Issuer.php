fc33d57d8a5d085df9ea538423e453f2d251bad9
Hugo Batalha Moreno <hugomb123@gmail.com>
2026-04-06 20:40:50 -0300

    🐛 fix(model): enable factory support in Issuer model using HasFactory

--- app/Models/Issuer.php
+++ app/Models/Issuer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 /**
  * Class Issuer
@@ -12,6 +13,8 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  */
 class Issuer extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['corporate_name', 'cnpj'];
 
     /**
