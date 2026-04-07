741e3c1d6db850d8c0e16c713edb0211714aede2
Hugo Batalha Moreno <hugomb123@gmail.com>
2026-04-07 16:41:35 -0300

    📝 feat(form_request): add request validation for creating obligations

---
+++ app/Http/Requests/StoreObligationRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * Class StoreObligationRequest
+ *
+ * Handles validation for creating obligations.
+ */
+class StoreObligationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false; //ACL via middleware
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'operation_id' => 'required|exists:operations,id',
+            'title' => 'required|string|max:255',
+            'due_date' => 'required|date',
+            'status' => 'required|in:PENDING,COMPLETED,CANCELLED',
+        ];
+    }
+}
