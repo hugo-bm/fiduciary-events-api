c67210ab3c1334d4e63ecd16da798216e0804c1f
Hugo Batalha Moreno <hugomb123@gmail.com>
2026-04-07 20:31:08 -0300

    ✨ feat(request): implement validation rules for UpdateOperationRequest

---
+++ app/Http/Requests/UpdateOperationRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateOperationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return ture; // Authorized via middleware
+    }
+
+    protected function prepareForValidation(): void
+    {
+        if ($this->operation_code) {
+            $this->merge([
+                'asset_code' => strtoupper($this->operation_code),
+            ]);
+        }
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'asset_code' => 'sometimes|string|max:50|unique:operations,asset_code,' . $this->route('operation')->id,
+            'operation_type' => 'sometimes|in:DEBENTURE,CRI,CRA',
+        ];
+    }
+}
