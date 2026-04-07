20639455a239be2f9a06dc481dd6aa431742857c
Hugo Batalha Moreno <hugomb123@gmail.com>
2026-04-07 08:46:48 -0300

    ✨ feat(enum): implement canAccess method in UserRoleEnum to check allowed roles

--- app/Enums/UserRoleEnum.php
+++ app/Enums/UserRoleEnum.php
@@ -12,4 +12,20 @@ enum UserRoleEnum: string
     case ADMIN = 'ADMIN';
     case ANALYST = 'ANALYST';
     case AUDITOR = 'AUDITOR';
+
+    /**
+     * Determine if the current role is allowed to access based on a list of permitted roles.
+     *
+     * This method checks whether the current enum instance exists within
+     * the provided array of allowed roles.
+     *
+     * @param UserRoleEnum[] $allowedRoles Array of roles allowed to access
+     *
+     * @return bool True if the current role is in the allowed list, false otherwise
+     */
+    public function canAccess(array $allowedRole): bool
+    {
+        return in_array($this, $allowedRole, true);
+    }
 }
+
