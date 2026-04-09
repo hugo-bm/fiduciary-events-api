5f93d7b1d88031b5ab79311b6f114214cbca11a3
Hugo Batalha Moreno <hugomb123@gmail.com>
2026-04-08 22:22:59 -0300

    ✨ feat(exception): simplify exception handling to focus on 404 and 500 responses

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,8 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\ApiKeyAuthMiddleware;
 use App\Http\Middleware\RoleMiddleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use App\Support\RequestLogger;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -22,5 +24,31 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        // 404
+        $exceptions->render(function (NotFoundHttpException $e, $request) {
+            RequestLogger::log('warning', 'Route not found');
+
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Resource not found',
+            ], 404);
+        });
+
+        // 500
+        $exceptions->render(function (\Throwable $e, $request) {
+
+            if (app()->runningInConsole()) {
+                \Log::error($e);
+                return;
+            }
+
+            RequestLogger::log('error', 'Unhandled exception', [
+                'exception' => $e->getMessage(),
+            ]);
+
+            return response()->json([
+                'status' => 'error',
+                'message' => 'An internal error occurred. Please try again later.',
+            ], 500);
+        });
     })->create();
